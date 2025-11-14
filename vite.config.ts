f4d54ef667f3f471fd652f38a2b57a54bb32aef7
kihak12 <jeremyperret43@gmail.com>
2025-11-14 14:34:29 +0100

    fix workflow error

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/HRNet-Openclassrooms-Projet-14/',
 })
